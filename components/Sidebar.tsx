77679c0e378eae6cf900e84772e900f93555f13e
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-01 17:43:31 +0300

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -55,8 +55,8 @@ const LinkItems: Array<LinkItemProps> = [
   
   { name: 'الرئيسية',url:'/', icon: FiHome },
   { name: 'أبحث عن عقار',url:'/search', icon: FaSearch },
-  { name:' عقارات للبيع ',url:'/search?porpose=for sale', icon: BsFillHouseAddFill },
-    { name:' عقارات للإيجار ',url:'/search?porpose=for rent', icon: BsFillHouseAddFill },
+  { name:' عقارات للبيع ',url:'/search?porpose=for sale', icon: BsFillHouseFill },
+    { name:' عقارات للإيجار ',url:'/search?porpose=for rent', icon: BsFillHouseFill },
   { name: ' أضف عقارك مجاناً',url:'/create', icon: FiPlus },
   // { name: '',url:'/agencies', icon: FiStar },
   // { name: 'الإعدادات',url:'/', icon: FiSettings },
