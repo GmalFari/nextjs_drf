ca7030c762b33f0371dbb24f3481d659d51f20e9
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-03 15:19:07 +0300

    Update MainSearch.jsx

--- components/MainSearch.jsx
+++ components/MainSearch.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
-import { Box,Button } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import {
     FormControl,
     FormLabel,
