acc411b8c2fd36c2cb96f0cf3ac72dcefc32e89d
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-05 17:23:06 +0300

    Update PropertyTable.jsx

--- components/property/PropertyTable.jsx
+++ components/property/PropertyTable.jsx
@@ -75,6 +75,65 @@ const PropertyTable =()=>{
     </Tfoot>
   </Table>
 </TableContainer>
+<TableContainer display={['none','block']} dir='rtl' maxW='100%'>
+
+  <Table variant='striped'>
+    
+    <Thead>
+      
+    </Thead>
+    <Tbody>
+    <Tr>
+        <Td>المدينه</Td>
+        <Td>صنعاء</Td>
+        
+      </Tr>
+      <Tr>
+        <Td>الحي / المنطق</Td>
+        <Td>سعوان </Td>
+        
+      </Tr>
+      <Tr>
+        <Td>عدد الغرف </Td>
+        <Td>٤ غرف نوم</Td>
+        
+      </Tr>
+      <Tr>
+        <Td>عدد الحمامات </Td>
+        <Td> حمامين</Td>
+        
+      </Tr>
+      <Tr>
+        <Td>مساحة البناء</Td>
+        <Td>20 م٢ </Td>
+        
+      </Tr>
+      <Tr>
+        <Td>مساحة الأرض
+</Td>
+        <Td>22 م٢ </Td>
+        
+      </Tr>
+      <Tr>
+        <Td>عدد الطوابق</Td>
+        <Td>طابقين</Td>
+        
+      </Tr>
+      <Tr>
+        <Td> عمر البناء
+ </Td>
+        <Td>قيد الإنشاء</Td>
+        
+      </Tr>
+      
+    </Tbody>
+    <Tfoot>
+      
+        
+      
+    </Tfoot>
+  </Table>
+</TableContainer>
 </Flex>
     </>
   )
