17152f46a9b9b5dd93154916906d49d0ae3b540d
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-06 23:18:43 +0300

    Create aboutus.js

---
+++ pages/profile/aboutus.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import { useState } from 'react'
+const AboutUs = () => {
+  return(
+      <Box>
+    لبنة هاوس الموقع الرائد لتسويق العقارات في كافة مناطق ومدن
+    الجمهورية اليمنيه 
+    يقدم خدماته للأفراد
+    والشركات ويتيح إمكانية البحث عن
+    العقار والوصول للعملاء
+    بكل سهولة ، وهو ما يحقق لأصحاب العقارات
+    والمكاتب العقارية والمطورين أفضل النتائج
+    </Box>
+  
+  )
+
+}
+
+export default AboutUs;
