647e8a4c9f838aedf3fa7ab0f5de38b7a9840add
GmalFari <gmalfari926@gmail.com>
2023-06-15 00:14:57 +0300

    a

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import myLogo from "../assets/images/logo2.png"
 import { Img,Text } from '@chakra-ui/react'
 const OurLogo = () => {
-  const imgSrc ="https://fortestmimd.pythonanywhere.com/media/media/logo1.png"
-     /*  {imgSrc && 
-    <Img src={imgSrc} width="141px" 
-     height="60px" />      } */
+  const imgSrc ="https://www.pythonanywhere.com/user/fortestmimd/files/home/fortestmimd/realestate_restapi/mediafiles/logo1.png"
+     
   return (
     <>
 
-   
-    {imgSrc && 
+   {imgSrc && 
+    <Img src={imgSrc} width="141px" 
+     height="60px" />      } 
+    {!imgSrc && 
    <Text color='#006275'> لبنة بروبيرتي </Text>
 }
     </>
