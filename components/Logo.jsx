2633e93e3d0e7e9132c5ab7b5d0ec3f21f9ddc21
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-15 01:14:35 +0300

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import myLogo from "../assets/images/logo2.png"
 import { Img,Text } from '@chakra-ui/react'
 const OurLogo = () => {
-  const imgSrc ="https://www.pythonanywhere.com/user/fortestmimd/files/home/fortestmimd/realestate_restapi/mediafiles/logo1.png"
-     
+  const imgSrc ="http://fortestmimd.pythonanywhere.com/media/media/logo1.png"
   return (
     <>
 
