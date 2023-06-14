308968cb579522d852461011e7fe7e46921127b4
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-14 14:48:48 +0300

    Update Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -3,13 +3,14 @@ import myLogo from "../assets/images/logo2.png"
 import { Img } from '@chakra-ui/react'
 const OurLogo = () => {
   const imgSrc ="https://fortestmimd.pythonanywhere.com/media/media/logo1.png"
-     
+     /*  {imgSrc && 
+    <Img src={imgSrc} width="141px" 
+     height="60px" />      } */
   return (
     <>
 
-   {imgSrc && <Img src={imgSrc} width="141px" 
-     height="60px" />      }
-    {!imgSrc && 
+   
+    {imgSrc && 
     'لبنة بروبيرتي '
 }
     </>
