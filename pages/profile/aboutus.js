e2016f7d77462522e3a27f357e0149e8db1d9fbc
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-20 17:44:16 +0300

    Update aboutus.js

--- pages/profile/aboutus.js
+++ pages/profile/aboutus.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box,Text,Center } from '@chakra-ui/react'
+import { Box,Text,Center,Divider } from '@chakra-ui/react'
 import { useState } from 'react'
 const AboutUs = () => {
   return(
@@ -7,7 +7,7 @@ const AboutUs = () => {
       <Box ms='10px' me='10px' maxW={'400px'} mb="300px" height={"80rm"} display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"} mt="100px">
       <Box mb="30px"><Text fontSize={"xl"} fontWeight={"bold"} as="h1" textAlign={"center"} >  من نحن</Text>
 </Box>
-  <Box>
+  <Box mt={4} mb={4} >
   <Text maxW="500px">
   لبنة بروبرتي الموقع الرائد لتسويق العقارات في كافة مناطق ومدن
     الجمهورية اليمنيه 
@@ -21,11 +21,13 @@ const AboutUs = () => {
   </Box>
   <Box mb="30px"><Text fontSize={"xl"} fontWeight={"bold"} as="h1" textAlign={"center"} >   أهدافنا</Text>
 </Box>
+      <Divider/>
   <Box>
   <Text maxW="500px">
     توفير بيئة اَمنة وسهلة لكل من يريد بيع أوشراء العقارات في الجمهورية اليمنية .
   </Text>
   </Box>
+      <Divider/>
 <Box mb="30px"><Text fontSize={"xl"} fontWeight={"bold"} as="h1" textAlign={"center"} >   رؤيتنا</Text>
 </Box>
   <Box>
