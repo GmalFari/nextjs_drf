7f5234cec17b29a5db35fc64b3aa1a5b74e3f0f1
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-22 20:18:46 +0300

    Update [id].js

--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -10,7 +10,7 @@ import {
 } from '@chakra-ui/react';
 import { fetchApi,baseUrl } from '../../utils/fetchApi'
 
-const OrderCard = ({orderDetail}) => {
+const OrderCard = ({orderDetail:{req_order_title,property_description,property_town}}) => {
  alert('hgg')
   return (
     <Center py={6}>
@@ -31,8 +31,8 @@ const OrderCard = ({orderDetail}) => {
           pos={'relative'}>
           <Image
             src={
-              'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
-            }
+              'https://upcdn.io/kW15bRn/raw/No_Image_Available.jpg9'
+                    }
             layout={'fill'}
           />
         </Box>
@@ -43,19 +43,16 @@ const OrderCard = ({orderDetail}) => {
             fontWeight={800}
             fontSize={'sm'}
             letterSpacing={1.1}>
-            Blog
+            تفاصيل الطلب
           </Text>
           <Heading
             color={useColorModeValue('gray.700', 'white')}
             fontSize={'2xl'}
             fontFamily={'body'}>
-            Boost your conversion rate
-          </Heading>
+            {req_order_title && req_order_title}
+               </Heading>
           <Text color={'gray.500'}>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum.
+              {property_description && property_description}
           </Text>
         </Stack>
         <Stack mt={6} direction={'row'} spacing={4} align={'center'}>
