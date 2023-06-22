37444ce627bbcebd172cac900f6282ef6c1677ee
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-22 20:44:10 +0300

    Create CustDetail.jsx

---
+++ components/CustDetail.jsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  Center,
+  Heading,
+  Text,
+  Stack,
+  Avatar,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react';
+const CustDetail =()=>{
+  return(
+    <>
+<Stack mt={6} direction={'row'} spacing={4} align={'center'}>
+          <Avatar
+            src={'https://avatars0.githubusercontent.com/u/1164541?v=4'}
+            alt={'Author'}
+          />
+          <Stack direction={'column'} spacing={0} fontSize={'sm'}>
+            <Text fontWeight={600}>Achim Rolle</Text>
+            <Text color={'gray.500'}>Feb 08, 2021 · 6min read</Text>
+          </Stack>
+  </Stack>
+    </>
+)
+}
+export default CustDetail;
