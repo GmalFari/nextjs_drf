2809e55c99f8a2f14a9c4acef0a7bb77138e1690
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-23 22:22:09 +0300

    Update orders.js

--- pages/orders.js
+++ pages/orders.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console
+/* eslint-disable no-console */
 import { Card, CardHeader, CardBody, CardFooter,
       useColorMode,useColorModeValue,Switch } from '@chakra-ui/react'
 import { Stack,StackDivider,Button,Box,Heading,Text,Badge, } from '@chakra-ui/react';
