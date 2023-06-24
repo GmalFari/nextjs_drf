f4ed6c856d263651dfeba2e08eb0ea6804a75e12
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-24 20:59:33 +0300

    Update ExUserInfo.jsx

--- components/accounts/ExUserInfo.jsx
+++ components/accounts/ExUserInfo.jsx
@@ -11,8 +11,8 @@ const  ExUserInfo=({ownerId})=> {
   const {user,authTokens} = useContext(AuthContext);
   const [phonenumber,setPhonenumber]=useState()
   const[userDetail,setUserDetail]=useState({
-    first_name:"",
-    last_name:"",
+    
+    name:"",
     email:"",
     phonenumber:"",
   });
@@ -50,9 +50,9 @@ useEffect(()=>{
   <CardHeader>
     <Flex spacing='2'>
       <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-        <Avatar name={`${userDetail.name && userDetail.first_name} ${userDetail.last_name && userDetail.last_name}`}/>
+        <Avatar name={`${userDetail.name && userDetail.name} ${userDetail.name && userDetail.name}`}/>
         <Box>
-          <Heading size='sm'>{userDetail.first_name&& userDetail.first_name} {userDetail.last_name &&userDetail.last_name} </Heading>
+          <Heading size='sm'>{userDetail.name&& userDetail.name} {userDetail.name &&userDetail.name} </Heading>
         {userDetail.name && <Text>{userDetail.name} </Text>}
         </Box>
       </Flex>
