6b68edd551b467a4618034cf12a2b7aaff2704fd
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-06-25 16:52:52 +0300

    Update ImageScrollbar.jsx

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -69,7 +69,7 @@ const ImageScrollbar = ({data,coverPhoto}) => {
         {defaultImg &&
         <ScrollMenu position={'initial'} LeftArrow={LeftArrow}
          RightArrow={RightArrow} style={{overflow:"scroll"}}>
-        {data && data.map((item) =>(
+        {data.length >=1 ? data.map((item) =>(
             <Box       
                     border={item === defaultImg?"4px solid #127578":"none"}
                     key={item.id} width="100px" height="100px" itemID={item.id}
@@ -87,7 +87,7 @@ const ImageScrollbar = ({data,coverPhoto}) => {
                 />
             </Box>
             
-        ))}
+        )):null}
         </ScrollMenu>
         }
         </Box>
