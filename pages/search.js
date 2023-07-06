9930d20f04a98f7bd894700879e95e614a34f258
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-07-06 17:30:35 +0300

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -51,13 +51,13 @@ const Search = ({data,query}) => {
     };
     useEffect(() => {
       setOnFilter(false) 
-      if(!onFilter){
-        if(currentPage < 1){
-          setCurrentPage(1)
-      }
-      if(currentPage > itemsCount){
-          setCurrentPage(itemsCount)
-      }
+     // if(!onFilter){
+      //  if(currentPage < 1){
+        //  setCurrentPage(1)
+   //   }
+     // if(currentPage > itemsCount){
+      //    setCurrentPage(itemsCount)
+    //  }
       const path = router.pathname;
       const {query } = router;  
       // clear filter data
@@ -84,34 +84,44 @@ const Search = ({data,query}) => {
         });
        }
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [currentPage,searchValue]);
+      }, [searchValue]);
    
-  /*
-  useEffect(() => {
-        
-          if(currentPage < 1){
-              setCurrentPage(1)
-          }
-          if(currentPage > itemsCount){
-              setCurrentPage(itemsCount)
-          }
-          const path = router.pathname;
-          const {query } = router;  
-          query["page"] = currentPage
-          router.push({pathname:path,query})
-          
-          console.log(query['property_title'])
-          axios.get(`https://fortestmimd.pythonanywhere.com/api/list-properties/?search=${query['search']}`)
-            .then((response) => {
-              setPageCount(response.data.count)
-              setProperties(response.data.results);
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-          
-        }, []);
-      */
+  nFilter(false) 
+      if(!onFilter){
+        if(currentPage < 1){
+          setCurrentPage(1)
+      }
+      if(currentPage > itemsCount){
+          setCurrentPage(itemsCount)
+      }
+      const path = router.pathname;
+      const {query } = router;  
+      // clear filter data
+      Object.keys(query).forEach(key => {
+        delete query[key]
+      })
+     // query["search"] = searchValue
+       query["page"] = currentPage
+
+      router.push({pathname:path,query});
+       const data = axios.get(`https://fortestmimd.pythonanywhere.com/api/list-properties/`,{
+        params:{
+          ...query
+        }
+       })
+        .then((response) => {
+
+          setPageCount(response.data.count)
+          setOnFilter(false)
+          setProperties(response.data.results);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+       }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, [currentPage]);
+   
     const listingsH = [properties.map((property) =>(
                <HorizonalCard   property={property} key={property.id} /> 
                    ))]
