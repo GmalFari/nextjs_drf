5f4a3de34803ad766bb138828e576fd216eb47c6
GmalFari <gmalfari926@gmail.com>
2023-07-12 20:50:35 +0300

    a

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -1,47 +1,48 @@
-import { writeFileSync } from 'fs';
-import {globby} from "globby"
-import prettier from 'prettier';
+// import { writeFileSync } from 'fs';
+// import {globby} from "globby"
+// import prettier from 'prettier';
  
-async function generate() {
-  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
-  const pages = await globby([
-    'pages/*.js',
-    'data/**/*.mdx',
-    '!data/*.mdx',
-    '!pages/_*.js',
-    '!pages/api',
-    '!pages/404.js',
-  ]);
+// async function generate() {
+//   const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+//   const pages = await globby([
+//     'pages/*.js',
+//     'data/**/*.mdx',
+//     '!data/*.mdx',
+//     '!pages/_*.js',
+//     '!pages/api',
+//     '!pages/404.js',
+//   ]);
  
-  const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${pages
-          .map((page) => {
-            const path = page
-              .replace('pages', '')
-              .replace('data', '')
-              .replace('.js', '')
-              .replace('.mdx', '');
-            const route = path === '/index' ? '' : path;
+//   const sitemap = `
+//     <?xml version="1.0" encoding="UTF-8"?>
+//     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+//         ${pages
+//           .map((page) => {
+//             const path = page
+//               .replace('pages', '')
+//               .replace('data', '')
+//               .replace('.js', '')
+//               .replace('.mdx', '');
+//             const route = path === '/index' ? '' : path;
  
-            return `
-              <url>
-                  <loc>${`https://libna-property.vercel.app${route}`}</loc>
-              </url>
-            `;
-          })
-          .join('')}
-    </urlset>
-    `;
+//             return `
+//               <url>
+//                   <loc>${`https://libna-property.vercel.app${route}`}</loc>
+//               </url>
+//             `;
+//           })
+//           .join('')}
+//     </urlset>
+//     `;
  
-  const formatted = prettier.format(sitemap, {
-    ...prettierConfig,
-    parser: 'html',
-  });
+//   const formatted = prettier.format(sitemap, {
+//     ...prettierConfig,
+//     parser: 'html',
+//   });
  
-  // eslint-disable-next-line no-sync
-  writeFileSync('public/sitemap.xml', formatted);
-}
+//   // eslint-disable-next-line no-sync
+//   writeFileSync('public/sitemap.xml',
+//    JSON.stringify(formatted));
+// }
  
-generate();
+// // generate();
