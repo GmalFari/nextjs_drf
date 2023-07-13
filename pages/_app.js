c388081f1e97276c49a7df08ebb3fedf87dc314b
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-07-14 02:08:42 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -48,12 +48,16 @@ function MyApp({Component,pageProps,router}){
       <Chakra theme={theme} >
 
           <AuthProvider >
+        {loading?<Box padding='6' boxShadow='lg' bg='white'>
+  <SkeletonCircle size='10' />
+  <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
+</Box>:
         <Layout >
         
                     
                         <Component {...pageProps} />
         
-                    </Layout>
+                    </Layout>}
             
             </AuthProvider>
         </Chakra>
