df091f45dc565894a5f9b6fb724ad8fbd4249021
GmalFari <72351012+GmalFari@users.noreply.github.com>
2023-07-13 22:38:54 +0300

    Update HorizonalCard.jsx

--- components/HorizonalCard.jsx
+++ components/HorizonalCard.jsx
@@ -94,7 +94,8 @@ const HorizonalCard = (
                     top=' 0'
                      left =' 0'
                       bottom='0'
-                      zIndex='99'
+                    right='0'
+                      zIndex='9999'
                       backgroundColor='rgba(0,0,0,0,0.6)'
                     ></Box>
                 <Img src={coverPhoto ? coverPhoto:defaultImg} 
